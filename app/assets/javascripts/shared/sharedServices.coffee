7b4c7d021c144090f58ae43d67a111a1559c71ff
Wenson Hsieh <whsieh@berkeley.edu>
2015-03-12 21:48:53 -0300

    Created topics API and connected topics survey page with server.

--- app/assets/javascripts/shared/sharedServices.coffee
+++ app/assets/javascripts/shared/sharedServices.coffee
@@ -1,5 +1,12 @@
 shares = angular.module("SharedServices", [])
-shares.service("SharedRequests", () ->
-    # TODO Implement calls to the server API.
+shares.service("SharedRequests", ($http) ->
+    SERVER_API_PREFIX = "/api/v1/"
+    this.getTopics = (callback) ->
+        $http.get("#{SERVER_API_PREFIX}topics").success((data, status) ->
+            if status == 200 then callback(data) else callback(null)
+        ).error(() ->
+            callback(null)
+        )
+
     return # Required to prevent returning the last object.
 )
---
+++ spec/controllers/topics_controller_spec.rb
@@ -0,0 +1,38 @@
+require "spec_helper"
+require "json"
+
+describe TopicsController do
+    before :each do
+        @topicA = Topic.create(:name => "TopicA", :icon => "/some/path/topic_a.png")
+        @topicB = Topic.create(:name => "TopicB", :icon => "/some/path/topic_b.png")
+        @topicC = Topic.create(:name => "TopicC", :icon => "/some/path/topic_c.png")
+        @topicD = Topic.create(:name => "TopicD", :icon => "/some/path/topic_d.png")
+        @topicE = Topic.create(:name => "TopicE", :icon => "/some/path/topic_e.png")
+        @lastTopicId = @topicE.id
+    end
+
+    it "should return all topics" do
+        get "get_all"
+        responseObject = JSON.parse(response.body).sort { |u, v| u["id"] <=> v["id"] }
+        expect(responseObject.length).to equal(5)
+        expect(responseObject[0]["name"]).to eq("TopicA")
+        expect(responseObject[1]["name"]).to eq("TopicB")
+        expect(responseObject[2]["name"]).to eq("TopicC")
+        expect(responseObject[3]["name"]).to eq("TopicD")
+        expect(responseObject[4]["name"]).to eq("TopicE")
+    end
+
+    it "should return a specific topic by id" do
+        get "get_by_id", :id => "1"
+        responseObject = JSON.parse(response.body)
+        expect(responseObject["name"]).to eq("TopicA")
+        expect(responseObject["id"]).to eq(1)
+        expect(responseObject["icon"]).to eq("/some/path/topic_a.png")
+    end
+
+    it "should return an empty object if no an id has no corresponding topic" do
+        get "get_by_id", :id => "100"
+        responseObject = JSON.parse(response.body)
+        expect(responseObject["error"]).to equal(true)
+    end
+end
