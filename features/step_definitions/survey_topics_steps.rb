307be7a01d6932617a0fc2206d94697986b2b2f8
Lucas Yan <lucasyan@berkeley.edu>
2015-03-13 22:18:13 -0700

    Tests

--- features/step_definitions/survey_topics_steps.rb
+++ features/step_definitions/survey_topics_steps.rb
@@ -1,25 +1,8 @@
-Given /I am on the "Survey Topic Checkboxes" page/ do
-    visit '/#topics'
-end
-
 When /I click (.*)/ do |topics|
     topics.split(", ").each do |topic_string|
         topic = topic_string[1..-2]
         topic.gsub!(" ", "_")
         page.find("##{topic.downcase}").click()
-    end
-end
-
-Then /the "(.*)" checkbox should( not)? be checked/ do |topic, should_not_be_checked|
-    topic.gsub!(" ", "_")
-    expect(page.find("##{topic.downcase}_checkbox", :visible => false).checked?).to be(!should_not_be_checked)
-end
-
-Then /I should( not)? see "(.*)"/ do |should_not_see, content|
-    if should_not_see
-        expect(page).not_to have_text(content)
-    else
-        expect(page).to have_text(content)
     end
 end
 
