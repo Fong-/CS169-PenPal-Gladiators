1d98bd2b1b1caa4b61bf51b75d7da3a4d8f579f8
Wenson Hsieh <whsieh@berkeley.edu>
2015-03-13 21:17:41 -0300

    Made startPage require /startpage instead of /startPage.

--- app/assets/javascripts/startPage.js
+++ app/assets/javascripts/startPage.js
@@ -2,6 +2,6 @@
 //= require angular-ui-bootstrap
 //= require angular-route
 //= require_tree ./shared
-//= require_tree ./startPage
+//= require_tree ./startpage
 
 app = angular.module("StartPage", ["ui.bootstrap", "ngRoute", "SharedServices", "StartPageServices", "Login", "SurveyTopics"])
