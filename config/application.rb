d9dcdc181f93551e4d7bd4bc5f56fd1df5f3ba1e
Kevin Wu <kevinwu.business@gmail.com>
2015-03-13 23:55:41 -0700

    Added compiled assets.

--- config/application.rb
+++ config/application.rb
@@ -54,7 +54,9 @@ module CS169PenpalGladiators
     config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
+    config.assets.precompile += %w( *.js *.css )
     config.assets.enabled = true
+    config.serve_static_assets = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
