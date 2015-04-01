d403c476c3fe3721baedb65f9bf3d8df567e0001
Kevin Wu <kevinwu.business@gmail.com>
2015-03-31 19:40:19 -0700

    Added tests for arena and conversation specs.

--- spec/models/conversation_spec.rb
+++ spec/models/conversation_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe Conversation do
-  pending "add some examples to (or delete) #{__FILE__}"
+    context "when using a conversation instance" do
+        before :each do
+            @conversation = Conversation.create! :title => "Happy feet"
+            @post1 = @conversation.posts.create! :text => "Lalalala"
+            @post2 = @conversation.posts.create! :text => "Good conversation"
+        end
+
+        it "should return the most recent post" do
+            expect(@conversation.recent_post).to eq(@post2)
+        end
+
+        it "should return the appropriate update time" do
+            expect(@conversation.timestamp).to eq(@conversation.updated_atc)
+        end
+    end
 end
