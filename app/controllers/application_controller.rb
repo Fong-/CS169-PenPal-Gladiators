a904555e87ebc35553dd21ce692f8654ddf73b20
Wenson Hsieh <whsieh@berkeley.edu>
2015-04-02 16:14:04 -0700

    Tweaked ApplicationController.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
-include AccessTokenHelper
-
 class ApplicationController < ActionController::Base
     def homepage
+        # TODO Move to angular controller.
         render :file => "public/homePage.html"
     end
 end
