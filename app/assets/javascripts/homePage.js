1ff1c66158e1f3dbca61d44134efc146c1722b02
Nicholas Fong <Fong-@users.noreply.github.com>
2015-04-02 23:26:30 -0700

    Update require_tree in homePage.js

--- app/assets/javascripts/homePage.js
+++ app/assets/javascripts/homePage.js
@@ -4,5 +4,6 @@
 //= require_tree ./shared
 //= require_tree ./sidebar
 //= require_tree ./homepage
+//= require_tree ./profile
 
 app = angular.module("HomePage", ["ui.bootstrap", "ngRoute", "SharedServices", "Profile", "Sidebar"])
