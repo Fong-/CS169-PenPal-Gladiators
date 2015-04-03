eaa7669b3d58c3dbc3d7486e9ee5a85454bf2911
Nicholas Fong <nickfong@users.noreply.github.com>
2015-04-03 10:08:54 -0700

    Save to profile UID instead of current user, update cucumber

--- features/step_definitions/app_steps.rb
+++ features/step_definitions/app_steps.rb
@@ -68,7 +68,7 @@ Given /^I am on (?:the|a|my) (.*?) page$/ do |page_name|
         when "survey" then pending "No route here!"
         when "user profile" then pending "no check for user profile!"
         when "home" then "/home"
-        when "profile" then "/home/#/profile"
+        when "profile" then "/home/#/profile/1"
         when "conversation" then pending "Check that I am on the conversation page."
         else raise "Could not navigate to the #{page_name} page."
         end
@@ -86,9 +86,14 @@ end
 
 Then /I should be on the (.*?) page/ do |page_name|
     case page_name
-    when "survey" then pending "no check for survey!"
-    when "profile" then page.should have_content "My Position on the Political Spectrum"
-    when "home" then pending "Check that I am on the home page."
-    else raise "No check for the #{page_name} page."
+    when "survey"
+        pending "no check for survey!"
+    when "profile"
+        puts page.html
+        page.should have_content "My Position on the Political Spectrum"
+    when "home"
+        pending "Check that I am on the home page."
+    else
+        raise "No check for the #{page_name} page."
     end
 end
