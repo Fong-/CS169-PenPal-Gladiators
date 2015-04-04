801cd8ad1df38d73e18bb2ec85542038732dcc5b
Nicholas Fong <Fong-@users.noreply.github.com>
2015-04-03 19:59:38 -0700

    Update navigation_bar.html to make cuke pass, other cuke bugfixes

--- features/step_definitions/user_profile_steps.rb
+++ features/step_definitions/user_profile_steps.rb
@@ -10,10 +10,19 @@ Then /^I should be able to select that I am "(.*?)"$/ do |position|
     choose(position)
 end
 
-Given /^there is a series of five radio buttons ranging from "(.*?)" to "(.*?)" to "(.*?)"$/ do |liberal, moderate, conservative|
-    page.should have_content liberal
-    page.should have_content moderate
-    page.should have_content conservative
+Then /^I should see the text "(.*?)"$/ do |text|
+    expect(page.html).to have_text(text)
+end
+
+#Given /^there is a series of five radio buttons ranging from "(.*?)" to "(.*?)" to "(.*?)"$/ do |liberal, moderate, conservative|
+Given /^there is a series of radio buttons corresponding to a political "(.*?)"$/ do |spectrum|
+#    page.should have_content liberal
+#    page.should have_content moderate
+#    page.should have_content conservative
+    page.should have_selector(spectrum)
+#    page.should have_selector(liberal)
+#    page.should have_selector(moderate)
+#    page.should have_selector(conservative)
 end
 
 Given /^"(.*?)" has a political hero of "(.*?)"$/ do |user, hero|
