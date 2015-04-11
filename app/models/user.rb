221baa755eea3cae759d4bfca11ed332c5e901fb
Lucas Yan <lucasyan@berkeley.edu>
2015-04-11 02:30:31 -0700

    Matching algorithm and tests. Call /api/v1/matches?id=<userid> to get a list of matches

--- app/models/user.rb
+++ app/models/user.rb
@@ -70,4 +70,68 @@ class User < ActiveRecord::Base
         self.political_spectrum = params[:political_spectrum]
         save
     end
+
+    def matches
+        all_users = User.all
+        sorted_list = []
+        for other_user in all_users
+            if other_user.id != self.id
+                match_data = self.score_user(other_user)
+                sorted_list.push({:user => other_user, :match_data => match_data})
+            end
+        end
+        return sorted_list.sort { |e1, e2| e1[:match_data][:score] <=> e2[:match_data][:score] } .reverse.last(5)
+    end
+
+    def score_user(other_user)
+        max = 0
+        max_topic = nil
+        
+        common_topics = self.find_common_topics(other_user)
+        for topic in common_topics
+            topic_score = self.find_topic_score(other_user, topic)
+            if max < topic_score
+                max = topic_score
+                max_topic = topic
+            end
+        end
+        return {:score => max, :topic => max_topic }
+    end
+
+    def find_common_topics(other_user)
+        my_topics = self.find_topics().to_set
+        other_user_topics = other_user.find_topics().to_set
+        return my_topics & other_user_topics
+    end
+
+    def find_topics()
+        topics = []
+        responses = UserSurveyResponse.where({:user_id => self.id})
+        for response in responses
+            topics.push(response.survey_response.survey_question.topic.id)
+        end
+        return topics
+    end
+
+    def find_topic_score(other_user, topic)
+        sum = 0
+        my_responses_hash = self.build_hash()
+        
+        other_responses = UserSurveyResponse.where({:user_id => other_user.id})
+        for other_response in other_responses
+            if other_response.survey_response.survey_question.topic.id == topic
+                sum += ResponseWeight.find_weight(other_response.survey_response.id, my_responses_hash[other_response.survey_response.survey_question.id.to_s])
+            end
+        end
+        return sum
+    end
+
+    def build_hash()
+        my_responses_hash = {}
+        my_responses = UserSurveyResponse.where({:user_id => self.id})
+        for my_response in my_responses
+            my_responses_hash[my_response.survey_response.survey_question.id.to_s] = my_response.survey_response.id
+        end
+        return my_responses_hash
+    end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,6 +3,167 @@ require 'spec_helper'
 
 describe UsersController, :type => :controller do
 
+    context "when finding matches" do
+        before :each do
+            users = [
+                {
+                    :email => "ben@bitdiddle.com",
+                    :password => "bitsrocks",
+                    :username => "ben",
+                    :avatar => "/path/to/ben.png",
+                    :political_blurb => "I like Ron Bitdiddle",
+                    :political_hero => "Ron Bitdiddle",
+                    :political_spectrum => 2 },
+                {
+                    :email => "generic@email.com",
+                    :password => "asdfasdf",
+                    :username => "generic",
+                    :avatar => "/path/to/generic.png",
+                    :political_blurb => "Go generic party!",
+                    :political_hero => "Generic George",
+                    :political_spectrum => 1 },
+                {
+                    :email => "bob@schmitt.com",
+                    :password => "imboring",
+                    :username => "bob",
+                    :avatar => "/path/to/bob.png",
+                    :political_blurb => "Bob for county clerk!",
+                    :political_hero => "ME!",
+                    :political_spectrum => 3 },
+                {
+                    :email => "genericasiankid@gmail.com",
+                    :password => "password",
+                    :username => "generickid",
+                    :avatar => "/path/to/generickid.png",
+                    :political_blurb => "Bob embezzles from widget farmers!",
+                    :political_hero => "Not Bob!",
+                    :political_spectrum => 1 },
+                {
+                    :email => "rosenthal@policy.com",
+                    :password => "publicpolicy",
+                    :username => "rosenthal",
+                    :avatar => "/path/to/rosenthal.png",
+                    :political_blurb => "Discuss your view on PenPal Gladiators!",
+                    :political_hero => "Thomas Jefferson",
+                    :political_spectrum => 2 }
+            ]
+
+            users.each do |u|
+                User.create!(u)
+            end
+
+            ############################################################
+            # Survey Topics, Questions, and Responses
+            ############################################################
+
+            responses = [
+                { :text => "Yes", :index => 0 },
+                { :text => "No", :index => 1}
+            ]
+
+            topics = [
+                { :name => "Climate", :icon => "/assets/topic_icons/climate.png"},
+                { :name => "Education", :icon => "/assets/topic_icons/education.png"},
+                { :name => "Economy", :icon => "/assets/topic_icons/money.png"},
+                { :name => "Technology", :icon => "/assets/topic_icons/technology.png"},
+                { :name => "LGBT Rights", :icon => "/assets/topic_icons/lgbt.png"},
+                { :name => "Immigration", :icon => "/assets/topic_icons/immigration.png"},
+                { :name => "Foreign Policy", :icon => "/assets/topic_icons/international.png"},
+                { :name => "Religion", :icon => "/assets/topic_icons/religion.png"},
+                { :name => "Philosophy", :icon => "/assets/topic_icons/philosophy.png"},
+                { :name => "Criminal Law", :icon => "/assets/topic_icons/justice.png"},
+            ]
+
+            topics.each do |t|
+                topic = Topic.create!(t)
+
+                survey_question1 = topic.survey_questions.create(:text => "Do you hate #{topic.name}?", :index => 1)
+                survey_question2 = topic.survey_questions.create(:text => "Do you care about #{topic.name}?", :index => 3)
+                created_responses = []
+                
+                responses.each do |r|
+                    verb1 = r[:text] == "Yes" ? "hate" : "don't hate"
+                    verb2 = r[:text] == "Yes" ? "care" : "don't care"
+                    actual_response1 = r.clone
+                    actual_response2 = r.clone
+                    actual_response1[:summary_text] = "I #{verb1} #{topic.name}."
+                    actual_response2[:summary_text] = "I #{verb2} about #{topic.name}."
+                    created_responses.push(survey_question1.survey_responses.create(actual_response1))
+                    created_responses.push(survey_question2.survey_responses.create(actual_response2))
+                end
+                
+                ResponseWeight.create({:response1_id => created_responses[0].id, :response2_id => created_responses[1].id, :weight => 5})
+                ResponseWeight.create({:response1_id => created_responses[0].id, :response2_id => created_responses[2].id, :weight => 1})
+                ResponseWeight.create({:response1_id => created_responses[0].id, :response2_id => created_responses[3].id, :weight => 1})
+                ResponseWeight.create({:response1_id => created_responses[1].id, :response2_id => created_responses[2].id, :weight => 1})
+                ResponseWeight.create({:response1_id => created_responses[1].id, :response2_id => created_responses[3].id, :weight => 1})
+                ResponseWeight.create({:response1_id => created_responses[2].id, :response2_id => created_responses[3].id, :weight => 1})
+
+            end
+
+            #More complicated responses
+            responses = [
+                {
+                    :text => "Education in the US is the best",
+                    :summary_text => "I believe that education in the US is the best.",
+                    :index => 2
+                },
+                {
+                    :text => "Need to fund education less",
+                    :summary_text => "I believe that we need to fund education less.",
+                    :index => 1
+                },
+                {
+                    :text => "Need more focus on STEM",
+                    :summary_text => "I believe that there needs to be more focus on STEM.",
+                    :index => 0
+                },
+                {
+                    :text => "Too many college grads, need to raise tuition fee",
+                    :summary_text => "I believe that there are too many college gradudates, therefore, we need to raise the tuition fees.",
+                    :index => 3
+                }
+            ]
+
+            education = Topic.find_by_name("Education")
+            survey_question = education.survey_questions.create(:text => "What's your view on US higher education?", :index => 2)
+            responses.each do |r|
+                survey_question.survey_responses.create(r)
+            end
+
+            ############################################################
+            # Users <-> survey responses through user survey responses
+            ############################################################
+
+
+            ben = User.find_by_email("ben@bitdiddle.com")
+            response = Topic.find_by_name("Climate").survey_questions.find_by_index(1).survey_responses.find_by_index(0)
+            UserSurveyResponse.create(:user => ben, :survey_response => response)
+
+            nick = User.find_by_email("generic@email.com")
+            response = Topic.find_by_name("Philosophy").survey_questions.find_by_index(1).survey_responses.find_by_index(1)
+            UserSurveyResponse.create(:user => nick, :survey_response => response)
+
+            bob = User.find_by_email("bob@schmitt.com")
+            response = Topic.find_by_name("Philosophy").survey_questions.find_by_index(1).survey_responses.find_by_index(0)
+            UserSurveyResponse.create(:user => bob, :survey_response => response)
+
+            wenson = User.find_by_email("genericasiankid@gmail.com")
+            response = Topic.find_by_name("Education").survey_questions.find_by_index(1).survey_responses.find_by_index(1)
+            UserSurveyResponse.create(:user => wenson, :survey_response => response)
+
+            rosenthal = User.find_by_email("rosenthal@policy.com")
+            response = Topic.find_by_name("Climate").survey_questions.find_by_index(1).survey_responses.find_by_index(1)
+            UserSurveyResponse.create(:user => rosenthal, :survey_response => response)
+        end
+        
+        it "should return the correct matches" do
+            get "matches", :id => User.find_by_email("ben@bitdiddle.com").id
+            responseObject = JSON.parse(response.body)
+            expect(responseObject.first["user"]["id"]).to eq User.find_by_email("rosenthal@policy.com").id
+        end
+    end
+    
     context "when accessing a user's profile information" do
         before :each do
             controller.stub(:check_access_token).and_return(true)
