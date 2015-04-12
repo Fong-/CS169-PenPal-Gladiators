86707ceed7fda2becb91939be86030b822945c02
Wenson Hsieh <whsieh@berkeley.edu>
2015-04-12 02:23:29 -0700

    Implemented cucumber, rspec tests for conversation page.

--- spec/models/conversation_spec.rb
+++ spec/models/conversation_spec.rb
@@ -3,10 +3,12 @@ require 'spec_helper'
 describe Conversation do
     context "when using a conversation instance" do
         before :each do
+            ben = User.create! :email => "ben@bitdiddle.com", :password => "asdfasdf"
+            bob = User.create! :email => "bob@schmitt.com", :password => "asdfasdf"
             @conversation = Conversation.create! :title => "Happy feet"
-            @post1 = @conversation.posts.create! :text => "Lalalala"
+            @post1 = @conversation.posts.create! :text => "Lalalala", :author => ben
             Timecop.freeze Time.now
-            @post2 = @conversation.posts.create! :text => "Good conversation"
+            @post2 = @conversation.posts.create! :text => "Good conversation", :author => bob
             Timecop.return
         end
 
@@ -16,6 +18,13 @@ describe Conversation do
 
         it "should return the appropriate update time" do
             expect(String(@conversation.timestamp)).to eq(String(@post2.updated_at))
+        end
+
+        it "should have the appropriate response object" do
+            result = @conversation.response_object
+            expect(result[:id]).to eq(@conversation.id)
+            expect(result[:title]).to eq(@conversation.title)
+            expect(result[:posts].length).to eq(@conversation.posts.length)
         end
     end
 end
