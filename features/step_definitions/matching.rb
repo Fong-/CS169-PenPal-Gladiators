605098a7b271883eca49ebd4ce9a81d05ac6a692
Nicholas Fong <Fong-@users.noreply.github.com>
2015-04-14 11:50:19 -0700

    Add pending step definitions for matching

---
+++ features/step_definitions/matching.rb
@@ -0,0 +1,12 @@
+Given(/^that the algorithm matches me with "(.*?)", "(.*?)", "(.*?)", "(.*?)", and "(.*?)"$/) do |match1, match2, match3, match4, match5|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see the option to initiate Gladiation with "(.*?)"$/) do |user|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^that "(.*?)" invited me to Gladiate$/) do |user|
+  pending # express the regexp above with the code you wish you had
+end
+
