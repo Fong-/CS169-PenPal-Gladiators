f7bd96bde4cf585cf6c5eb8f97b5ded72a4d8e70
Kevin Wu <kevinwu.business@gmail.com>
2015-04-15 14:37:42 -0700

    Removed unnecessary route.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,3 @@
 class ApplicationController < ActionController::Base
-    def homepage
-        # TODO Move to angular controller.
-        render "public/homePage", :formats => :html
-    end
+
 end
