8f7c605bd0d05a57c1ccb88adc4eb41a9acfc4a5
Lucas Yan <lucasyan@berkeley.edu>
2015-04-15 11:42:12 -0700

    Working state for matching
    Conflicts:
    	spec/models/user_spec.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150411023351) do
+ActiveRecord::Schema.define(:version => 20150415181140) do
 
   create_table "arenas", :force => true do |t|
     t.integer  "user1_id"
