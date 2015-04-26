c679eba1f6ec78fea30a19c3d2013977d1f9edc1
Kevin Wu <kevinwu.business@gmail.com>
2015-04-26 01:57:05 -0700

    Added backend routes and handler to create a new conversation.

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -24,6 +24,25 @@ class ConversationsController < ApplicationController
             arenas = Arena.for_users(user, other_user)
 
             if arenas.length == 0
+                render_error(:resource_not_found, :resource => :arena)
+            end
+
+            arena = arenas[0]
+            conversation = arena.conversations.create :title => "Untitled Conversation"
+            render :json => { :id => conversation.id }
+        else
+            render_error(:resource_not_found, :resource => :other_user)
+        end
+    end
+
+    def create
+        user = @token_results[:user]
+        other_user = User.find_by_id(params[:user_id])
+
+        if other_user.present?
+            arenas = Arena.for_users(user, other_user)
+
+            if arenas.length == 0
                 render_error(:resource_not_found, :resource => :arena) and return
             end
 
