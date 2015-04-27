6c91a84a1ff70ed7ab7236ba63b19bb2bffc895f
Kevin Wu <kevinwu.business@gmail.com>
2015-04-27 11:14:24 -0700

    Fixed a few errors after rebasing.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150425025047) do
+ActiveRecord::Schema.define(:version => 20150426202521) do
 
   create_table "arenas", :force => true do |t|
     t.integer  "user1_id"
