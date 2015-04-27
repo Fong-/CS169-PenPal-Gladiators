de886f94e58449a464a4ec0a1fc79e81b097fce6
Wenson Hsieh <whsieh@berkeley.edu>
2015-04-26 18:02:37 -0700

    Progress on invitations back end API.

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,11 @@ CS169PenpalGladiators::Application.routes.draw do
     get "/api/v1/conversation/:id" => "conversations#get_by_id"
     post "/api/v1/post/create/:conversation_id" => "posts#create"
     post "/api/v1/post/edit/:post_id" => "posts#edit"
+    get "/api/v1/invites/outgoing" => "invites#outgoing"
+    get "/api/v1/invites/incoming" => "invites#incoming"
+    post "/api/v1/invite/send/:to_user" => "invites#invite"
+    post "/api/v1/invite/accept/:from_user" => "invites#accept"
+    post "/api/v1/invite/reject/:from_user" => "invites#reject"
     # Public routes
     get "/" => "pages#homepage"
     get "/login" => "pages#startpage"
