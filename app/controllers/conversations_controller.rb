a9ceecd87c0282b5b63595b2e1c37024b4718e2a
Wenson Hsieh <whsieh@berkeley.edu>
2015-04-28 19:58:28 -0700

    Fixed conversation polling bug and removed redundant code from the conversation controller.

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -36,25 +36,6 @@ class ConversationsController < ApplicationController
         end
     end
 
-    def create
-        user = @token_results[:user]
-        other_user = User.find_by_id(params[:user_id])
-
-        if other_user.present?
-            arenas = Arena.for_users(user, other_user)
-
-            if arenas.length == 0
-                render_error(:resource_not_found, :resource => :arena) and return
-            end
-
-            arena = arenas[0]
-            conversation = arena.conversations.create :title => "Untitled Conversation"
-            render :json => { :id => conversation.id }
-        else
-            render_error(:resource_not_found, :resource => :other_user)
-        end
-    end
-
     def edit_title
         unless did_extract_arguments_from_params
             render_error :resource_not_found and return
